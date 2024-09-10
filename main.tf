f40f3bc69aead0354386054680ddcac0c0981239
Aws-devops <awsdevops1943@gmail.com>
2024-09-10 15:49:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-0e53db6fd757e38c7" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
