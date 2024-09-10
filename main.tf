7cb6035fa05d17002f530798ee65fd9408239fb9
Aws-devops <awsdevops1943@gmail.com>
2024-09-10 16:49:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "TF-Instance" {
   ami           = "ami-0e53db6fd757e38c7" # us-west-2
   instance_type = "t2.micro"
   tags = {
